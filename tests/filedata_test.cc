da9b91d20459487f90fe1bf415dd9b7514e45619
hit9 <hit9@icloud.com>
2023-10-09 14:59:54 +0800

    Add more tests and benchmark

--- tests/filedata_test.cc
+++ tests/filedata_test.cc
@@ -1,3 +1,4 @@
+#include <catch2/benchmark/catch_benchmark_all.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <fstream>
 #include <string>
@@ -62,3 +63,107 @@ TEST_CASE("DecodeNaive", "[DecodeNaive file data #1]") {
   REQUIRE(simple_utf8::DecodeNaive(s, p) > 0);
   REQUIRE(p == target);
 }
+
+TEST_CASE("Decode", "[Decode file data #2]") {
+  // read file
+  std::string s;
+  std::getline(std::ifstream("tests/data/UTF-8-cn.txt"), s, '\0');
+  auto target = ReadCodepointsFromFile("tests/data/UTF-8-cn-codepoints.txt");
+  REQUIRE(s.size() > 0);
+  REQUIRE(target.size() > 0);
+  std::u32string p(simple_utf8::CountCodes(s), 0);
+  REQUIRE(simple_utf8::Decode(s, p) > 0);
+  REQUIRE(p == target);
+}
+
+TEST_CASE("DecodeNaive", "[DecodeNaive file data #2]") {
+  // read file
+  std::string s;
+  std::getline(std::ifstream("tests/data/UTF-8-cn.txt"), s, '\0');
+  auto target = ReadCodepointsFromFile("tests/data/UTF-8-cn-codepoints.txt");
+  REQUIRE(s.size() > 0);
+  REQUIRE(target.size() > 0);
+  std::u32string p(simple_utf8::CountCodes(s), 0);
+  REQUIRE(simple_utf8::DecodeNaive(s, p) > 0);
+  REQUIRE(p == target);
+}
+
+TEST_CASE("Decode", "[Decode file data #3]") {
+  // read file
+  std::string s;
+  std::getline(std::ifstream("tests/data/UTF-8-random-1.txt"), s, '\0');
+  auto target =
+      ReadCodepointsFromFile("tests/data/UTF-8-random-1-codepoints.txt");
+  REQUIRE(s.size() > 0);
+  REQUIRE(target.size() > 0);
+  std::u32string p(simple_utf8::CountCodes(s), 0);
+  REQUIRE(simple_utf8::Decode(s, p) > 0);
+  REQUIRE(p == target);
+}
+
+TEST_CASE("DecodeNaive", "[DecodeNaive file data #3]") {
+  // read file
+  std::string s;
+  std::getline(std::ifstream("tests/data/UTF-8-random-1.txt"), s, '\0');
+  auto target =
+      ReadCodepointsFromFile("tests/data/UTF-8-random-1-codepoints.txt");
+  REQUIRE(s.size() > 0);
+  REQUIRE(target.size() > 0);
+  std::u32string p(simple_utf8::CountCodes(s), 0);
+  REQUIRE(simple_utf8::DecodeNaive(s, p) > 0);
+  REQUIRE(p == target);
+}
+
+// {{{ Decoding Benchmark
+
+TEST_CASE("Decoding Benchmark", "[Decode benchmark #1]") {
+  // read file
+  std::string s;
+  std::getline(std::ifstream("tests/data/UTF-8-demo.txt"), s, '\0');
+  auto target = ReadCodepointsFromFile("tests/data/UTF-8-demo-codepoints.txt");
+  REQUIRE(s.size() > 0);
+  REQUIRE(target.size() > 0);
+  std::u32string p(simple_utf8::CountCodes(s), 0);
+
+  // Benchmark
+  BENCHMARK("Decode benchmark (UTF-8-demo.txt)") {
+    return simple_utf8::Decode(s, p);
+  };
+  BENCHMARK("DecodeNaive benchmark (UTF-8-demo.txt)") {
+    return simple_utf8::DecodeNaive(s, p);
+  };
+}
+
+TEST_CASE("Decoding Benchmark", "[Decode benchmark #2]") {
+  // read file
+  std::string s;
+  std::getline(std::ifstream("tests/data/UTF-8-cn.txt"), s, '\0');
+  auto target = ReadCodepointsFromFile("tests/data/UTF-8-cn-codepoints.txt");
+  REQUIRE(s.size() > 0);
+  REQUIRE(target.size() > 0);
+  std::u32string p(simple_utf8::CountCodes(s), 0);
+
+  // Benchmark
+  BENCHMARK("Decode benchmark (chinese)") { return simple_utf8::Decode(s, p); };
+  BENCHMARK("DecodeNaive benchmark (chinese)") {
+    return simple_utf8::DecodeNaive(s, p);
+  };
+}
+
+TEST_CASE("Decoding Benchmark", "[Decode benchmark #3]") {
+  // read file
+  std::string s;
+  std::getline(std::ifstream("tests/data/UTF-8-random-1.txt"), s, '\0');
+  auto target = ReadCodepointsFromFile("tests/data/UTF-8-random-1-codepoints.txt");
+  REQUIRE(s.size() > 0);
+  REQUIRE(target.size() > 0);
+  std::u32string p(simple_utf8::CountCodes(s), 0);
+
+  // Benchmark
+  BENCHMARK("Decode benchmark (random)") { return simple_utf8::Decode(s, p); };
+  BENCHMARK("DecodeNaive benchmark (random)") {
+    return simple_utf8::DecodeNaive(s, p);
+  };
+}
+
+// }}}
